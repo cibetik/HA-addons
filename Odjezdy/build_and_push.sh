ed7f2448dab919b1ca6921be7f54be2f345f8b8f
cibetik <79955953+cibetik@users.noreply.github.com>
2024-05-30 14:14:43 +0200

    Automatizations

---
+++ Odjezdy/build_and_push.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if tag argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <tag>"
+  exit 1
+fi
+
+TAG=$1
+ARCHITECTURES=("amd64" "aarch64" "armv7")
+BUILD_FROM_BASE="homeassistant"
+CONFIG_FILE="config.yaml"
+
+# Check if config.yaml exists
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "$CONFIG_FILE does not exist."
+  exit 1
+fi
+
+# Update version in config.yaml (cross-platform compatible)
+sed -i.bak "s/version: \".*\"/version: \"${TAG}\"/" "$CONFIG_FILE" && rm "${CONFIG_FILE}.bak"
+
+for ARCH in "${ARCHITECTURES[@]}"; do
+  # Define the build argument
+  BUILD_FROM="${BUILD_FROM_BASE}/${ARCH}-base:latest"
+  # Define the image name
+  IMAGE_NAME="cibetik/odjezdy-${ARCH}:${TAG}"
+  
+  # Build the Docker image
+  docker buildx build --build-arg BUILD_FROM="${BUILD_FROM}" -t "${IMAGE_NAME}" .
+  
+  # Push the Docker image
+  docker push "${IMAGE_NAME}"
+done
